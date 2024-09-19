4a882e3518784e5e9c2a7832d67a5771f60a6a95
Álvaro Torres <alvaro.espindola.torres@gmail.com>
2024-09-19 16:25:01 -0600

    feat: implement new changes

---
+++ scripts/modal.js
@@ -0,0 +1,4 @@
+function clickModal(modalId) {
+  // Asegúrate de que el modal se esté inicializando correctamente con Bootstrap
+  $('#' + modalId).modal('show'); // Esto activará el modal con el ID pasado como argumento
+}
